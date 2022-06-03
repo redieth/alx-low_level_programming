6307ae9909c649e102714aeb556fa777f274fa6d
rediet <redidevt@gmail.com>
2022-06-03 22:21:42 +0000

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 48;
+	while (a <= 57)
+	{
+		putchar(a);
+		putchar(',');
+		putchar(' ');
+		a++;
+	}
+	putchar('\n');
+	return (0);
