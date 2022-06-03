365d6f3a39ee24f3b6086684bc4e7d5aade0eae1
rediet <redidevt@gmail.com>
2022-06-03 22:05:14 +0000

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+	
+	a = 48;
+	b = 97;
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 102)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
