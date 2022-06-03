8a66ae15609350e42d3099d19a4a2f6546156918
rediet <redidevt@gmail.com>
2022-06-03 21:41:47 +0000

    Task 6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a <= 9)
+	{
+		printf("%d", a);
+	}
+	printf('\n');
+	return (0);
+}
