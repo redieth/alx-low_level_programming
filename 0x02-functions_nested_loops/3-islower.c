edbde06704dda554b360458dfc8e61c0016c8f72
rediet <redidevt@gmail.com>
2022-06-07 14:14:32 +0000

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,8 @@
  *_islower - checks if a charchter passed is lowercase
  * @c: alphabet being checked
  *
- * Return: Always 0 (Success)
- */ 
+ * Return: Always 0
+ */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
