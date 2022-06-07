10112ed635c5377ccf1c69d40ed10e5b199e6662
rediet <redidevt@gmail.com>
2022-06-07 18:10:57 +0000

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int hr = 0;
+	int min = 0;
+	
+	while (hr <= 23)
+	{
+		while (min <= 59)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min / 10) + '0');
+		}
+	}
+}
