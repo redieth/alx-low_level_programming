1f0138eb24abd858cf2cfea88329736735fcc823
rediet <redidevt@gmail.com>
2022-06-07 19:48:35 +0000

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,19 +15,25 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			multiple = i * j;
-			if (multiple < 10)
+			if (j == 0)
 			{
 				_putchar(multiple + '0');
 			}
-			else if (multiple >= 10)
+			else if (multiple < 10)
 			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(multiple + '0');
+			}
+			else (multiple >= 10)
+			{
+				_putchar(' ');
 				_putchar((multiple / 10) + '0');
 				_putchar((multiple % 10) + '0');
 			}
 			if (j != 9)
 			{
 				_putchar(',');
-				_putchar(' ');
 			}
 		}
 		_putchar('\n');
