ef0c127377fa6401841812a83516eb58ebbdf568
rediet <redidevt@gmail.com>
2022-06-07 21:50:15 +0000

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	int i;
-	int t1 = 1;
-	int t2 = 2;
+	unsigned long int i;
+	unsigned long int t1 = 1;
+	unsigned long int t2 = 2;
 	int nextTerm = t1 + t2;
 	
 	printf("%d, %d", t1, t2);
 	for (i = 3; i <= 50; i++)
 	{
-		printf("%d, ", nextTerm);
+		printf("%d", nextTerm);
 		t1 = t2;
 		t2 = nextTerm;
 		nextTerm = t1 + t2;
