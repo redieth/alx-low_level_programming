39dbb4b0fd80dc6028ce029a8a02e33c831a1bbb
rediet <redidevt@gmail.com>
2022-06-07 12:45:50 +0000

    Task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10.c - print the lowercase alphabets 10 times
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
