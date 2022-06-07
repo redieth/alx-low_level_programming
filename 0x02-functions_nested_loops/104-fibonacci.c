47cb0e4af056cc546851a8d819684888dbc8764f
rediet <redidevt@gmail.com>
2022-06-07 22:57:00 +0000

    Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+/**
+ * main - prints the first 50 fibonacci numbers starting from 1 and 2
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int t1 = 1;
+	unsigned long int t2 = 2;
+	unsigned long int nextTerm = t1 + t2;
+	
+	printf("%lu, %lu, ", t1, t2);
+	for (i = 3; i <= 98; i++)
+	{
+		if (i == 98)
+		{
+			printf("%lu", nextTerm);
+		}
+		else
+		{
+			printf("%lu, ", nextTerm);
+		}
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+	printf("\n");
+	return (0);
+}
