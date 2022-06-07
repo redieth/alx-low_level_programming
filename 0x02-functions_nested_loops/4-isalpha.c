bb4d7562662206244f94118001647f0ef3c4b857
rediet <redidevt@gmail.com>
2022-06-07 15:16:19 +0000

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _isalpha - alphabetic character
- * @c: is parameter passed 
+ * @c: is parameter passed
  *
  * Return: Always 0
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >=65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
 	else
 		return (0);
