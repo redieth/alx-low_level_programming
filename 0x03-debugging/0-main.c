9380523d123f0faa9c57e06560641324135ae9de
rediet <redidevt@gmail.com>
2022-06-09 13:50:44 +0000

    Task 3

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 /**
  * main - test a function that prints if an integer positive or negative
  *
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
 	int i;
-
+	
 	i = 0;
 	positive_or_negative(i);
-
+	
 	return (0);
 }
