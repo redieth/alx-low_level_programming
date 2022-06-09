b16e674d37dacff16e489a5d073a8e3a097f3743
rediet <redidevt@gmail.com>
2022-06-09 12:12:06 +0000

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - test a function that prints if an integer positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
