3190525729f5b6c13d1e36cf0fd84526823a43bc
rediet <redidevt@gmail.com>
2022-06-09 23:56:20 +0000

    Task 5

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _putchar(char c);
-
 /**
  * print_number - like a hello world
+ *
  * @n: params an integer
+ *
  * Return: void
  */
 void print_number(int n)
@@ -26,6 +26,6 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-
+	
 	_putchar((n1 % 10) + '0');
 }
