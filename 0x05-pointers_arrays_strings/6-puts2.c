c10129ee9f3bbbe19c5144479081b1acfacc26cc
rediet <redidevt@gmail.com>
2022-06-13 23:13:20 +0000

    Pointers, Array and Strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,10 +6,11 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+	while (str[i] != '\0')
 	{
-		_putchar(*str);
-		str = str + 2;
+		_putchar(str[i]);
+		i = i + 2;
 	}
 	_putchar('\n');
 }
