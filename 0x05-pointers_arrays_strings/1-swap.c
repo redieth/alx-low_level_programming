c756f2d240db741dd27b29fd3be5fd0de63f3cec
rediet <redidevt@gmail.com>
2022-06-13 20:17:42 +0000

    Pointers, Array and Strings

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,7 +9,13 @@
  */
 void swap_int(int *a, int *b)
 {
-	*a = *a + *b;
+	/*
+	 * *a = *a + *b;
 	*b = *a - *b;
-	*a = *a - *b;
+	*a = *a - *b;*/
+	int temp;
+
+	*temp = *a;
+	*a = *b;
+	*b = temp;
 }
