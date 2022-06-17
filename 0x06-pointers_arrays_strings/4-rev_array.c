c1ea4ed02269976ec47a9defca1856c671d323dc
rediet <redidevt@gmail.com>
2022-06-17 00:18:27 +0000

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * reverse_array - reverse an array
+ * @a: the array
+ * @n: size
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+
+	int left = 0;
+	int right = n - 1;
+	
+	while (left < right)
+	{
+		temp = a[left];
+		a[left] = a[right];
+		a[right] = temp;
+		left++;
+		right--;
+	}
+}		
