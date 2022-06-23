181830faa4c7bb9d041eb883944989e4940d01c8
rediet <redidevt@gmail.com>
2022-06-23 12:55:05 +0000

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,14 +7,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
+	do {
 		if (*s == c)
 		{
 			return (s);
 		}
-		s++;
-	}
-
+	} while (*s++);
+	
 	return (0);
 }
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: the string
+ * @accept: inital segemnt
+ * Return: int 
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	
