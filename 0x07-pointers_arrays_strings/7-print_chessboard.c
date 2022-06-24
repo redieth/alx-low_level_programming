62a73461bf9a00b526e7d9c3258cbab8dd0aec53
rediet <redidevt@gmail.com>
2022-06-24 15:04:03 +0000

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_chessboard - print a chess board
+ * @a: dimension
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+
+	int i,j;
+
+	for (i = 0; i < 2; i++)
+	{
+		for(j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (i = 0; i < 4; i++)
+	{
+		for(j = 0; j < 8; j++)
+		{
+			printf(" ");
+		}
+		printf("\n");
+	}
+
+	for (i = 6; i < 8; i++)
+	{
+		for(j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
