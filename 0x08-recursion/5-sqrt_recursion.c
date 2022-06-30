d8144e47dc004bf5796f6ff9fdac825bf0b4fb4e
rediet <redidevt@gmail.com>
2022-06-30 12:02:17 +0000

    0x08. C - Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - return the natural square root a number.
+ * @n: the number
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * primeHelper - checks whether num is prime number
+ * @num: the number to checked
+ * @i: the divisor
+ * Return: 1 if prime 0 is not prime
+ */
+int helper(int num, int i)
+{
+	if (i < num)
+	{
+		if (num % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (primeHelper(num, i + 1));
+		}
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+/**
+ * is_prime_number - checks whether n is prime number
+ * @n: to be checked
+ * Return: returns 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (primeHelper(n, 2));
+	}
+
+}
