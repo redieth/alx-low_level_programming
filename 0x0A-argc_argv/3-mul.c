ce1b27a0716cca1efe46c90a4bd443e0dea1223d
rediet <redidevt@gmail.com>
2022-07-15 11:59:57 +0000

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main
+ * @argc: count
+ * @argv: arguments
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i, result = 1;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		result *= atoi(argv[i]);
+	}
+	printf("%d\n", result)
+	}
+	return (0);
+}
