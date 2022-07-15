060946bb712723a2a6b12b045f0c51ae2b088b45
rediet <redidevt@gmail.com>
2022-07-15 09:26:16 +0000

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: array of argument
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argc);
+	return (0);
+}
