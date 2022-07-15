0cdb5268067933952b20fc006b1b71a0f651020e
rediet <redidevt@gmail.com>
2022-07-15 11:21:00 +0000

    0x0A. C - argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: array of argument
  * Return: int
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
 		printf("%d\n", argc - 1);
