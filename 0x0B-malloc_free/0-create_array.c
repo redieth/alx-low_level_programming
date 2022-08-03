7175a157c16c4cca2212cd5298470de749385315
rediet <redidevt@gmail.com>
2022-08-03 12:45:49 +0000

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * create_array - create array
+ * @size: size
+ * @c: constant
+ * Return: char pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	S = malloc(sizeof(char) * size);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
+}
