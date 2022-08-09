0ed87bf2a8f466b6d2060bd850c80aa0f7ec6bd3
rediet <redidevt@gmail.com>
2022-08-09 07:21:32 +0000

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	range = (max - min) + 1;
-	p = malloc(sizeof(char) * range);
+	p = malloc(sizeof(int) * range);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i <= max; i++)
