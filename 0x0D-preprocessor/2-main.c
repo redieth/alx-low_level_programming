80de7d3decfc3f8daf682273621ba8e44eb45987
rediet <redidevt@gmail.com>
2022-08-10 23:39:12 +0000

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argv: arguments
+ * @argc: number of arguments
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
