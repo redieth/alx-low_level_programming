1d7e8dd7d5828114e7ff198d0751b66870ff43be
rediet <redidevt@gmail.com>
2022-08-17 22:30:58 +0000

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog struct
+ * @d:pointer to a dog struct
+ * @name: dog name
+ * @age: dog's age
+ * @owner: owner's name
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
