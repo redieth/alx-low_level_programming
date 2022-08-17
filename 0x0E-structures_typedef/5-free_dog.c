d327c636797fc10842a235b14c08f82e72e688fd
rediet <redidevt@gmail.com>
2022-08-17 23:31:15 +0000

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free memory of dog
+ * @d: pointer to dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
