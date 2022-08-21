8487ac817445bb8456b5120bb464fb24463a3c54
rediet <redidevt@gmail.com>
2022-08-21 20:54:39 +0000

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * premain - execute code before the main
+ * return - non return, void function
+ */
+void __attribute__ ((constructor)) premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
