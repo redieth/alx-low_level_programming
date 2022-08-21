5770e47f02cd07a14d0a1721c73e57e748cc7adb
rediet <redidevt@gmail.com>
2022-08-21 18:45:05 +0000

    0x10. C - Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,27 +1,22 @@
-#ifndef _VARIADIC_FUNCTIONS_H
-#define _VARIADIC_FUNCTIONS_H
+#ifndef VARIAFUNC
+#define VARIAFUNC
 #include <stdarg.h>
 #include <stdio.h>
 
 /**
- * struct fmt - Struct format
- * @f: type format
- * @p: print function
+ * struct args_t - contain the type and the function.
+ * @format: type format
+ * @function: print function
  */
-typedef struct fmt
+typedef struct args_t
 {
-	char *f;
-	void (*p)(va_list s);
-} my_fmt;
+	char *format;
+	void (*function)();
+} args_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _strlen_recursion(char *s);
-void print_c(va_list s);
-void print_i(va_list s);
-void print_f(va_list s);
-void print_s(va_list s);
 
 #endif
