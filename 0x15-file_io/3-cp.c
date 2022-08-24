d054860b93ada2857d6173102cbca50e764a826b
redieth <96776786+redieth@users.noreply.github.com>
2022-08-24 22:15:11 +0300

    0x15. C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,6 @@ void close_file(int fd);
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
- *
  * Return: A pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
